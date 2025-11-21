2b0b1a69653099ca595b9d07d8ce9c8b6cea22db
Kimberly Prijadi <prijadikimberly@gmail.com>
2025-11-21 16:25:08 -0500

    moved output data file

--- src/main/java/use_case/navigate/NavigateOutputData.java
+++ src/main/java/use_case/navigate/NavigateOutputData.java
@@ -1,18 +1,34 @@
 package use_case.navigate;
 
+import entity.Puzzle;
+
+/**
+ * Output data returned from the NavigateInteractor.
+ * Contains the updated story text and the direction the player moved.
+ */
 public class NavigateOutputData {
 
     private final String storyText;
     private final String direction;
+    private final String location;
+    private final Puzzle puzzle;
 
-    public NavigateOutputData(String storyText, String direction) {
+    public NavigateOutputData(String storyText, String direction, String location, Puzzle puzzle) {
         this.storyText = storyText;
-        this.direction = direction;}
+        this.direction = direction;
+        this.location = location;
+        this.puzzle = puzzle;
+    }
 
     public String getStoryText() {
-        return storyText;}
+        return storyText;
+    }
 
     public String getDirection() {
         return direction;
     }
+
+    public String getLocation () { return location; }
+
+    public Puzzle getPuzzle() { return puzzle; }
 }
